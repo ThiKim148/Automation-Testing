d3e7302fa2b021f29d3e4b26d454517ddae01f28
ThiKim148 <hothikim14102004@gmail.com>
2026-07-07 17:06:57 +0700

    add the script to delete account testing

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -6,11 +6,14 @@ from api.auth_client import AuthClient
 @pytest.fixture(scope="class", autouse=True)
 def clean_test_account(test_user):
 
+    print("===== CLEANUP START =====")
+
     response = AuthClient.delete_user(
         test_user["email"],
         test_user["password"]
     )
 
-    print("Cleanup:", response.text)
+    print("HTTP:", response.status_code)
+    print("BODY:", response.text)
 
     yield
