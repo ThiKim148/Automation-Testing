20512027b0e911cfaeee4916c502e775382db6cc
ThiKim148 <hothikim14102004@gmail.com>
2026-07-07 10:52:27 +0700

    Adjust the logic of the conftest file

--- conftest.py
+++ conftest.py
@@ -1,3 +1,5 @@
+import os
+import json
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
@@ -13,3 +15,14 @@ def driver():
     yield driver
 
     driver.quit()
+
+@pytest.fixture(scope="session")
+def get_data():
+    def _read_file(file_name):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        data_file_path = os.path.join(current_dir, "data", f"{file_name}.json")
+        
+        with open(data_file_path, "r", encoding="utf-8") as f:
+            return json.load(f)
+            
+    return _read_file
